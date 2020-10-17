03ebec7c1f1a38248e77806e6a21e70bd29946ba
ebonynon <sithumsandeepap@gmail.com>
2020-10-17 19:17:03 +0530

    addpost ~0

--- src/views/TeacherPage/setFeed.section.js
+++ src/views/TeacherPage/setFeed.section.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import authHeader from '../assets/jss/services/auth-header'
 import marked from 'marked'
 import ISimpleMDE from 'react-simplemde-v1'
 import Button from '@material-ui/core/Button'
+import Alert from '@material-ui/lab/Alert'
 import 'simplemde/dist/simplemde.min.css'
 
 export default function SetFeedSection(props) {
   const [textValue, setTextValue] = useState('')
+  const [alert, setAlert] = useState()
+  const [alertType, setAlertType] = useState()
+
+  const ID = props.location.pathname.split('/classfeed/')[1]
+
   const option = {}
 
   const onReady = instance => console.log(instance.value())
@@ -23,6 +31,34 @@ export default function SetFeedSection(props) {
     return { __html: rawMarkup }
   }
 
+const onPost = e => {
+  e.preventDefault()
+
+  const data = {
+    class_id: ID,
+    post_data: textValue,
+  }
+  console.log('D :: ', data)
+  axios
+  .post(`http://localhost:8082/api/teacher/addpost`, data, {
+    headers: authHeader(),
+  })
+  .then(res => {
+    setTextValue('')
+    //setValue({
+    //  student_uid: '',
+    //})
+    setAlertType('success')
+    setAlert(res.status)
+  })
+  .catch(err => {
+    setAlertType('error')
+    setAlert(err.message)
+    console.log('Error in AddStudent!')
+  })
+
+}
+
   return (
     <div
     //className={classes.root}
@@ -37,10 +73,11 @@ export default function SetFeedSection(props) {
         variant="contained"
         color="default"
         //startIcon={<SearchIcon />}
-        //onClick={onPost}
+        onClick={onPost}
       >
         Post
       </Button>
+      <Alert severity={`${alertType}`}>{alert}</Alert>
       <br />
       <br />
       <br />
