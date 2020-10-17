99e1551bc897597d70614bf791db4a498f081596
ebonynon <sithumsandeepap@gmail.com>
2020-10-17 15:21:22 +0530

    ISimpleMDE n marked

--- src/views/TeacherPage/setFeed.section.js
+++ src/views/TeacherPage/setFeed.section.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import marked from 'marked';
+import marked from 'marked'
 import ISimpleMDE from 'react-simplemde-v1'
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button'
 import 'simplemde/dist/simplemde.min.css'
 
 export default function SetFeedSection(props) {
-  const [textValue, setTextValue] = useState('');
+  const [textValue, setTextValue] = useState('')
   const option = {}
 
   const onReady = instance => console.log(instance.value())
@@ -19,30 +19,32 @@ export default function SetFeedSection(props) {
   }
 
   const getMarkdownText = () => {
-    var rawMarkup = marked(textValue, {sanitize: true});
-    return { __html: rawMarkup };
+    var rawMarkup = marked(textValue, { sanitize: true })
+    return { __html: rawMarkup }
   }
-  
+
   return (
-    <div 
+    <div
     //className={classes.root}
     >
-    <ISimpleMDE
-      option={option}
-      text={'Hello World!!!'}
-      onReady={onReady}
-      onEvents={onEvents}
-    />
-    <Button
-    variant="contained"
-    color="default"
-    //startIcon={<SearchIcon />}
-    //onClick={onPost}
-  >
-    Post
-  </Button>
-  <br/><br/><br/>
-  <div dangerouslySetInnerHTML={getMarkdownText()} />
-  </div>
+      <ISimpleMDE
+        option={option}
+        text={'Hello World!!!'}
+        onReady={onReady}
+        onEvents={onEvents}
+      />
+      <Button
+        variant="contained"
+        color="default"
+        //startIcon={<SearchIcon />}
+        //onClick={onPost}
+      >
+        Post
+      </Button>
+      <br />
+      <br />
+      <br />
+      <div dangerouslySetInnerHTML={getMarkdownText()} />
+    </div>
   )
 }
