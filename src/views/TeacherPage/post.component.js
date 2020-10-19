5d670c141e584bfeb28e1b7777938279df8da271
ebonynon <sithumsandeepap@gmail.com>
2020-10-19 09:55:58 +0530

    with maxWith mess

--- src/views/TeacherPage/post.component.js
+++ src/views/TeacherPage/post.component.js
@@ -19,9 +19,9 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     marginBottom: theme.spacing(2),
-    width: 'auto',
+    width: '100%',
     height: 'auto',
-    maxWidth: 1080,
+    //maxWidth: 1920,
     boxShadow:
       '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)',
   },
