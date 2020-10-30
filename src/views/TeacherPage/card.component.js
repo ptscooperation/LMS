42cbde44695ca4eee7249ef6642cbcb4648c2a39
ebonynon <sithumsandeepap@gmail.com>
2020-10-30 05:37:44 +0530

    remove teacher card icons

--- src/views/TeacherPage/card.component.js
+++ src/views/TeacherPage/card.component.js
@@ -52,7 +52,7 @@ export default function CardComponent(props) {
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <IconButton
+          {/* <IconButton
             color="inherit"
             aria-label="Edit the class"
             title="Edit the class"
@@ -73,7 +73,7 @@ export default function CardComponent(props) {
             }}
           >
             <EmailIcon />
-          </IconButton>
+          </IconButton> */}
           <IconButton
             color="inherit"
             aria-label="Add student"
